c45f9fe71f2f628d5a63c459bf282c7e4dc322fc
ANDY POLINAR <andygolez143@gmail.com>
2021-10-21 13:59:15 +0800

    Rating

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'midtermproject';
+
+  name = 'Rate The Avengers Endgame Movie';
+  rate = 0;
+
+  movTitle = 'Avengers EndGame';
+  par = 'Avengers: Endgame is a 2019 American superhero film based on the Marvel Comics superhero team the Avengers. ... In the film, the surviving members of the Avengers and their allies attempt to reverse the destruction caused by Thanos in Infinity War.';
+
+  bgk:string = "assets/images/123.jfif"
+  images:string = "assets/images/avengers.jpg";
+
 }
